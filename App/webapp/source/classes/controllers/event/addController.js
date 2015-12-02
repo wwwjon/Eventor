69f3e7cbae03e4f7352b25b7488862bb6bde38e3
wwwjon <wwjon@outlook.com>
2015-12-02 12:54:19 +0100

    guests

--- App/webapp/source/classes/controllers/event/addController.js
+++ App/webapp/source/classes/controllers/event/addController.js
@@ -3,30 +3,16 @@ define(['app/model/event'], function(Event) {
 
 	var EventAddController = function($scope, $location, EventRepository) {
 
-
 		this.scope = $scope;
-		this.scope.hours = this.createNumberSerie(0,1,23,2);
-		this.scope.minutes = this.createNumberSerie(0,5,59,2);
-		this.scope.event = new Event();
-		this.scope.mindate = new Date();
+		this.scope.event = {};
+		this.scope.minDate = new Date();
+		//this.scope.event.begin = new Date();
+		//this.scope.event.end = new Date();
 
 		this.scope.add = function(newEvent) {
-			/*newEvent.times.begin = new Date(
-				newEvent.times.begin.date.getFullYear(),
-				newEvent.times.begin.date.getMonth(),
-				newEvent.times.begin.date.getDate(),
-				newEvent.times.begin.time.hours,
-				newEvent.times.begin.time.minutes,
-				0
-			);
-			newEvent.times.end = new Date(
-				newEvent.times.end.date.getFullYear(),
-				newEvent.times.end.date.getMonth(),
-				newEvent.times.end.date.getDate(),
-				newEvent.times.end.time.hours,
-				newEvent.times.end.time.minutes,
-				0
-			); */
+
+			newEvent.times.begin = new Date(newEvent.times.begin);
+			newEvent.times.end = new Date(newEvent.times.end);
 
 			EventRepository.add(
 				newEvent,
